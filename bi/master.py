62e6de3614f35549e2125c6959cbc960dad28cb9
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-01-22 11:00:25 +0530

    Test push

--- bi/master.py
+++ bi/master.py
@@ -104,6 +104,7 @@ def main(configJson):
     print("55555555555555555555555555555555555555",messages_for_API)
     messages_for_API = json.dumps(messages_for_API)
     print("---------------------start ---------------------", messages_for_API)
+    print("---------------------Pipeline------------------")
     try:
         res = requests.put(url=initialMessageURL,data=messages_for_API)
     except Exception as e:
